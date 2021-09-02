525f5e1e711f0d220eb13f23f5986dec2946b01b
Michal Procházka <prochazka2003@gmail.com>
2021-09-02 15:28:05 +0200

    Well that was dumb

--- input.c
+++ input.c
@@ -26,8 +26,8 @@ kbdshortcut_t shortcuts[] = {
 	{ 0, SDL_SCANCODE_F8, player_stop },
 	{ 0, SDL_SCANCODE_F9, edit_octave_down },
 	{ 0, SDL_SCANCODE_F10, edit_octave_up },
-	{ 0, SDL_SCANCODE_F11, track_add_order },
-	{ 0, SDL_SCANCODE_F12, track_remove_order },
+	{ 0, SDL_SCANCODE_F11, track_remove_order },
+	{ 0, SDL_SCANCODE_F12, track_add_order },
 };
 
 #define n_shortcuts (sizeof(shortcuts) / sizeof(kbdshortcut_t))
--- menu/track.c
+++ menu/track.c
@@ -8,8 +8,8 @@ menu_t submenu_track = {
 	C(18), C(1), C(24), C(3), 3,
 	BLACK, WHITE, {
 	
-	{ 0, C(0), C(24), C(1), "Add order entry    [F11]" },
-	{ 0, C(1), C(24), C(1), "Remove order entry [F12]" },
+	{ 0, C(0), C(24), C(1), "Add order entry    [F12]" },
+	{ 0, C(1), C(24), C(1), "Remove order entry [F11]" },
 	{ 0, C(2), C(24), C(1), "Add/remove channels" },
 }};
 
